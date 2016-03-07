53bab1c70e05c0f2c853f1552140b22d7b07608f
EricRazor <eric.razorjr@gmail.com>
2016-03-07 14:13:23 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
-#write your code here
-
-def countdown
+#require 'pry'
+def countdown(countdown_output, countdown)
+	counter = 11
+	while counter > countdown_output
+		10.times do
+			puts "#{countdown} SECOND(S)!"
+			sleep 1
+			countdown -= 1
+			if countdown == 0 
+				break
+			end
+		end
+		puts "HAPPY NEW YEARS!"
+	end
 end
+
+countdown(10, 5)
